08a05d7d55579eb3418499cf93cbe7feee397fc7
O.K <okgit@protonmail.com>
2019-12-01 11:01:42 +0100

    Added exits

--- hastebin.py
+++ hastebin.py
@@ -9,5 +9,7 @@ response = requests.post('https://hastebin.com/documents', data=data)
 if response.status_code == 200:
     key = json.loads(response.text)['key']
     pyperclip.copy('https://hastebin.com/{}'.format(key))
+    exit(0)
 else:
     pyperclip.copy('hastebin response code: {}'.format(response.status_code))
+    exit(1)
