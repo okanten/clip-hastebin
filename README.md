9a19d58411572717bf279cce4d908af8ce03ea17
O.K <okgit@protonmail.com>
2019-12-01 10:42:33 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# hastebin.py
+
+Send your clipboard to hastebin with this quick and easy python script
+
+# Requirements
+
+requests, pyperclip, json - see requirements.txt
+
+# Usage
+
+Run it in your terminal (adding it to your scripts folder is a good idea) or use a keybind-manager to run the script.
---
+++ hastebin.py
@@ -0,0 +1,13 @@
+import requests
+import pyperclip
+import json
+
+data = pyperclip.paste()
+
+response = requests.post('https://hastebin.com/documents', data=data)
+
+if response.status_code == 200:
+    key = json.loads(response.text)['key']
+    pyperclip.copy('https://hastebin.com/{}'.format(key))
+else:
+    pyperclip.copy('hastebin response code: {}'.format(response.status_code))
