51f187f9f835db6f944a95168ad1b2db7106e7a9
O.K <okgit@protonmail.com>
2022-06-21 20:49:43 +0200

    Updated URL after Hastebin got aquired by toptal

--- hastebin.py
+++ hastebin.py
@@ -2,14 +2,16 @@ import requests
 import pyperclip
 import json
 
+BASE_URL = "https://www.toptal.com/developers/hastebin"
+
 data = pyperclip.paste()
 
-response = requests.post('https://hastebin.com/documents', data=data)
+response = requests.post(f'{BASE_URL}/documents', data=data)
 
 if response.status_code == 200:
     key = json.loads(response.text)['key']
-    pyperclip.copy('https://hastebin.com/{}'.format(key))
+    pyperclip.copy(f'{BASE_URL}/{key}')
     exit(0)
 else:
-    pyperclip.copy('hastebin response code: {}'.format(response.status_code))
+    pyperclip.copy(f'hastebin response code: {response.status_code}')
     exit(1)
